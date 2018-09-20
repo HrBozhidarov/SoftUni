d2e370274dafacaa1815dd5076480f2947537810
HrBozhidarov <h_bozhidarov@abv.bg>
2018-09-20 21:52:57 +0300

    C# web homework 1

---
+++ C# Web Development Basics/02. Lab HTTP Protocol/02. Validate URL/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Validate_URL
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var url = Console.ReadLine();
+                var decodeUrl = WebUtility.UrlDecode(url);
+
+                var uri = new Uri(decodeUrl);
+
+                var dic = new Dictionary<string, string>
+                {
+                    ["protocol"] = uri.Scheme,
+                    ["host"] = uri.Host,
+                    ["port"] = uri.Port.ToString(),
+                    ["path"] = uri.LocalPath,
+                    ["query"] = uri.Query == "" ? "" : uri.Query.Substring(1),
+                    ["fragment"] = uri.Fragment == "" ? "" : uri.Fragment.Substring(1)
+                };
+
+                if ((dic["protocol"] == "https" && int.Parse(dic["port"]) != 443) || (dic["protocol"] == "http" && int.Parse(dic["port"]) != 80) || string.IsNullOrEmpty(dic["host"]) || string.IsNullOrEmpty(dic["path"]))
+                {
+                    throw new Exception();
+                }
+
+                foreach (var item in dic)
+                {
+                    if (item.Value == "")
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid URL");
+            }
+        }
+    }
+}
---
+++ C# Web Development Basics/02. Lab HTTP Protocol/03. Request Parser/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Request_Parser
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var res = new Dictionary<string, HashSet<string>>();
+
+            var input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var splitInput = input.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var path = splitInput[0];
+                var method = splitInput[1];
+
+                if (!res.ContainsKey(path))
+                {
+                    res[path] = new HashSet<string>();
+                }
+
+                res[path].Add(method);
+            }
+
+            var request = Console.ReadLine().Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var methodRequest = request[0].ToLower().Trim();
+            var pathRequest = request[1].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0];
+
+            var statusCode = !res.ContainsKey(pathRequest) || !res[pathRequest].Contains(methodRequest) ? "404 NotFound" : "200 ok";
+
+            Console.WriteLine($"HTTP/1.1 {statusCode}");
+            Console.WriteLine($"Content-Length: {statusCode.Split()[1].Length}");
+            Console.WriteLine("Content-Type: text/plain" + Environment.NewLine);
+            Console.WriteLine($"{statusCode.Split()[1]}");
+        }
+    }
+}

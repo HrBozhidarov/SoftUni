bf398af9104b5518a804993a156d9875d8697c76
HrBozhidarov <h_bozhidarov@abv.bg>
2019-02-11 22:26:53 +0200

    Add linear data structures lab 1.

---
+++ Data Structures/01. Lab Linear Data Structures/Lists/ArrayList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    private const string IndexOutOfRangeMessage = "Index out of range exception.";
+    private const int InitialCapacity = 2;
+
+    private T[] innerArray;
+    private int capacity;
+
+    public ArrayList(int capacity = InitialCapacity)
+    {
+        this.innerArray = new T[capacity];
+        this.capacity = capacity;
+    }
+
+    public int Count { get; private set; }
+
+    public T this[int index]
+    {
+        get
+        {
+            IndexOutOfRange(index);
+
+            return this.innerArray[index];
+        }
+
+        set
+        {
+            IndexOutOfRange(index);
+
+            this.innerArray[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        ResizeIfNeeded();
+
+        this.innerArray[Count] = item;
+
+        this.Count++;
+    }
+
+    public T RemoveAt(int index)
+    {
+        IndexOutOfRange(index);
+
+        var returnElement = this.innerArray[index];
+        var newArr = new T[this.capacity];
+        var isFoundIndex = false;
+
+        for (int i = 0; i < this.Count; i++)
+        {
+            if (i == index)
+            {
+                isFoundIndex = true;
+
+                continue;
+            }
+
+            if (isFoundIndex)
+            {
+                newArr[i - 1] = innerArray[i];
+            }
+            else
+            {
+                newArr[i] = innerArray[i];
+            }
+        }
+
+        this.innerArray = newArr;
+        this.Count--;
+
+        return returnElement;
+    }
+
+    private void ResizeIfNeeded()
+    {
+        if (this.Count == this.capacity)
+        {
+            this.capacity *= InitialCapacity;
+            var newArray = new T[capacity];
+
+            for (int i = 0; i < this.innerArray.Length; i++)
+            {
+                newArray[i] = this.innerArray[i];
+            }
+
+            this.innerArray = newArray;
+        }
+    }
+
+    private void IndexOutOfRange(int index)
+    {
+        if (this.Count < index || index < 0)
+        {
+            throw new ArgumentOutOfRangeException(IndexOutOfRangeMessage);
+        }
+    }
+}
---
+++ Data Structures/01. Lab Linear Data Structures/Lists/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var a = new List<int>();
+
+        a.Add(1);
+        a.Add(2);
+        a[1] = 1;
+        foreach (var item in a)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+}

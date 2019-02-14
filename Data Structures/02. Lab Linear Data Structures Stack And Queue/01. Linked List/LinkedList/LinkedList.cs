9a147a5b52a5c4f2ed117a8817458266bd5e0424
HrBozhidarov <h_bozhidarov@abv.bg>
2019-02-14 15:38:47 +0200

    Add lab DS Queue and Stack.

---
+++ Data Structures/02. Lab Linear Data Structures Stack And Queue/01. Linked List/LinkedList/LinkedList.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    public class Node
+    {
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public Node Next { get; set; }
+    }
+
+    public Node Head { get; set; }
+
+    public Node Tail { get; set; }
+
+    public int Count { get; private set; }
+
+    public void AddFirst(T item)
+    {
+        Node old = this.Head;
+
+        this.Head = new Node(item);
+        this.Head.Next = old;
+
+        if (this.Count == 0)
+        {
+            this.Tail = this.Head;
+        }
+
+        this.Count++;
+    }
+
+    public void AddLast(T item)
+    {
+        Node old = this.Tail;
+        this.Tail = new Node(item);
+
+        if (this.Count == 0)
+        {
+            this.Head = this.Tail;
+        }
+        else
+        {
+            old.Next = this.Tail;
+        }
+
+        this.Count++;
+    }
+
+    public T RemoveFirst()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var value = this.Head.Value;
+        var afterFirst = this.Head.Next;
+        this.Head = afterFirst;
+
+        this.Count--;
+
+        return value;
+    }
+
+    public T RemoveLast()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T value = this.Tail.Value;
+
+        if (this.Count == 1)
+        {
+            value = this.Head.Value;
+            this.Head = null;
+            this.Tail = this.Head;
+        }
+        else
+        {
+            RemoveLastElementFromTail();
+        }
+
+        this.Count--;
+
+        return value;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var node = this.Head;
+
+        while (node != null)
+        {
+            yield return node.Value;
+            node = node.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private void RemoveLastElementFromTail()
+    {
+        Node parent = this.Head;
+
+        while (parent.Next != this.Tail)
+        {
+            parent = parent.Next;
+
+        }
+
+        parent.Next = null;
+        this.Tail = parent;
+    }
+}
---
+++ Data Structures/02. Lab Linear Data Structures Stack And Queue/02. Circular Queue/CircularQueue/CircularQueue.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+
+public class CircularQueue<T>
+{
+    private const int DefaultCapacity = 4;
+
+    private T[] elements;
+    private int startPointer;
+    private int endPointer;
+
+    public CircularQueue(int capacity = DefaultCapacity)
+    {
+        this.elements = new T[capacity];
+    }
+
+    public int Count { get; private set; }
+
+    public void Enqueue(T element)
+    {
+        if (this.Count == this.elements.Length)
+        {
+            Resize();
+        }
+
+        this.elements[endPointer] = element;
+        this.endPointer = (this.endPointer + 1) % this.elements.Length;
+  
+        this.Count++;
+    }
+
+    private void Resize()
+    {
+        var newElements = new T[2 * elements.Length];
+
+        CopyAllElements(newElements);
+
+        this.elements = newElements;
+        this.startPointer = 0;
+        this.endPointer = this.Count;
+    }
+
+    private void CopyAllElements(T[] newArray)
+    {
+        for (int i = 0; i < this.Count; i++)
+        {
+            var index = (this.startPointer + i) % this.Count;
+
+            newArray[i] = this.elements[index];
+        }
+    }
+
+    public T Dequeue()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var element = this.elements[startPointer];
+        this.elements[startPointer] = default(T);
+        this.startPointer = (this.startPointer + 1) % elements.Length;
+        this.Count--;
+
+        return element;
+    }
+
+    public T[] ToArray()
+    {
+        var arr = new T[this.Count];
+
+        this.CopyAllElements(arr);
+
+        return arr;
+    }
+}
+
+
+public class Example
+{
+    public static void Main()
+    {
+
+        CircularQueue<int> queue = new CircularQueue<int>();
+
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        int first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-7);
+        queue.Enqueue(-8);
+        queue.Enqueue(-9);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        queue.Enqueue(-10);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+
+        first = queue.Dequeue();
+        Console.WriteLine("First = {0}", first);
+        Console.WriteLine("Count = {0}", queue.Count);
+        Console.WriteLine(string.Join(", ", queue.ToArray()));
+        Console.WriteLine("---------------------------");
+    }
+}

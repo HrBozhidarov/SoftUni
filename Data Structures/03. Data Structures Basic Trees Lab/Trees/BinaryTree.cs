905a7e65855d4c469bdf785e60ae0850199e2f8a
HrBozhidarov <h_bozhidarov@abv.bg>
2019-02-18 17:44:05 +0200

    lab: basic trees.

---
+++ Data Structures/03. Data Structures Basic Trees Lab/Trees/BinaryTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.Value = value;
+        this.LeftChild = leftChild;
+        this.RightChild = rightChild;
+    }
+
+    public T Value { get; set; }
+
+    public BinaryTree<T> LeftChild { get; set; }
+
+    public BinaryTree<T> RightChild { get; set; }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + this.Value);
+
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.PrintIndentedPreOrder(indent + 2);
+        }
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.PrintIndentedPreOrder(indent + 2);
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachInOrder(action);
+        }
+
+        action(this.Value);
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachInOrder(action);
+        }
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachPostOrder(action);
+        }
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachPostOrder(action);
+        }
+
+        action(this.Value);
+    }
+}
---
+++ Data Structures/03. Data Structures Basic Trees Lab/Trees/Tree.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+
+        foreach (var child in children)
+        {
+            this.Children.Add(child);
+        }
+    }
+
+    public T Value { get; set; }
+
+    public List<Tree<T>> Children { get; set; }
+
+    public void Print(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + this.Value);
+
+        foreach (var child in this.Children)
+        {
+            child.Print(indent + 2);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(this.Value);
+
+        foreach (var child in this.Children)
+        {
+            child.Each(action);
+        }
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+        var result = new List<T>();
+
+        DFS(this, result);
+
+        return result;
+    }
+
+    private void DFS(Tree<T> node,List<T> result)
+    {
+        foreach (var child in node.Children)
+        {
+            DFS(child, result);
+        }
+
+        result.Add(node.Value);
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        var result = new List<T>();
+        var queue = new Queue<Tree<T>>();
+
+        BFS(result, queue);
+
+        return result;
+    }
+
+    private void BFS(List<T> result,Queue<Tree<T>> queue)
+    {
+        queue.Enqueue(this);
+
+        while (queue.Count > 0)
+        {
+            var currentNode = queue.Dequeue();
+
+            result.Add(currentNode.Value);
+
+            foreach (var child in currentNode.Children)
+            {
+                queue.Enqueue(child);
+            }
+        }
+    }
+}

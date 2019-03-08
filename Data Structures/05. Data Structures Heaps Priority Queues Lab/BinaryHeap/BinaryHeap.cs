e91c0f1b63c7cfacc4681fc98ca32444b218d7e1
HrBozhidarov <h_bozhidarov@abv.bg>
2019-03-08 11:55:38 +0200

    Add Data Structures Heaps Priority Queues Lab/05

---
+++ Data Structures/05. Data Structures Heaps Priority Queues Lab/BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public BinaryHeap()
+    {
+        this.heap = new List<T>();
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.heap.Count;
+        }
+    }
+
+    public void Insert(T item)
+    {
+        this.heap.Add(item);
+
+        this.HeapifyUp(this.Count - 1);
+    }
+
+    private void HeapifyUp(int index)
+    {
+        var parrentIndex = (index - 1) / 2;
+
+        if (parrentIndex < 0)
+        {
+            return;
+        }
+
+        if (this.heap[index].CompareTo(this.heap[parrentIndex]) > 0)
+        {
+            Swap(parrentIndex, index);
+
+            HeapifyUp(parrentIndex);
+        }
+    }
+
+    private void Swap(int parrentIndex, int childIndex)
+    {
+        var temp = this.heap[parrentIndex];
+        this.heap[parrentIndex] = this.heap[childIndex];
+        this.heap[childIndex] = temp;
+    }
+
+    public T Peek()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return this.heap[0];
+    }
+
+    public T Pull()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var result = this.heap[0];
+
+        Swap(0, this.Count - 1);
+
+        this.heap.RemoveAt(this.Count - 1);
+
+        this.HeapifyDown(0);
+
+        return result;
+    }
+
+    private void HeapifyDown(int index)
+    {
+        var leftIndex = (2 * index) + 1;
+        var rightIndex = (2 * index) + 2;
+
+        var compareIndex = GetIndex(leftIndex, rightIndex);
+
+        if (compareIndex > -1)
+        {
+            if (this.heap[index].CompareTo(this.heap[compareIndex]) < 0)
+            {
+                Swap(compareIndex, index);
+
+                HeapifyDown(compareIndex);
+            }
+        }
+    }
+
+    private int GetIndex(int leftIndex, int rigthIndex)
+    {
+        if (leftIndex>=this.Count && rigthIndex >= this.Count)
+        {
+            return -1;
+        }
+
+        if (leftIndex >= this.Count)
+        {
+            return rigthIndex;
+        }
+        else if (rigthIndex >= this.Count)
+        {
+            return leftIndex;
+        }
+
+        var result = this.heap[leftIndex].CompareTo(this.heap[rigthIndex]) > 0 ? leftIndex : rigthIndex;
+
+        return result;
+    }
+}
---
+++ Data Structures/05. Data Structures Heaps Priority Queues Lab/BinaryHeap/Heap.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr)
+    {
+        var length = arr.Length;
+
+        for (int i = length / 2; i >= 0; i--)
+        {
+            HeapifyDown(arr, i, length);
+        }
+
+        for (int i = length - 1; i > 0; i--)
+        {
+            Swap(arr, 0, i);
+
+            HeapifyDown(arr, 0, i);
+        }
+    }
+
+    private static void HeapifyDown(T[] arr, int parentIndex, int length)
+    {
+        while (parentIndex < length / 2)
+        {
+            var childIndex = (parentIndex * 2) + 1;
+
+            if (childIndex + 1 < length && IsGreater(arr, childIndex + 1, childIndex))
+            {
+                childIndex++;
+            }
+
+            if (!IsGreater(arr, childIndex, parentIndex))
+            {
+                break;
+            }
+
+            Swap(arr, childIndex, parentIndex);
+
+            parentIndex = childIndex;
+        }
+    }
+
+    private static void Swap(T[] arr, int childIndex, int parentIndex)
+    {
+        var temp = arr[childIndex];
+        arr[childIndex] = arr[parentIndex];
+        arr[parentIndex] = temp;
+    }
+
+    private static bool IsGreater(T[] arr, int parrentIndex, int currentIndex)
+    {
+        return arr[parrentIndex].CompareTo(arr[currentIndex]) > 0;
+    }
+}

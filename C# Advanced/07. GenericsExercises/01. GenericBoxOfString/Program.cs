0b7d331c60bb82fd6ef91b478bf0b89453a0c3b7
HrBozhidarov <hristo_bozhidarov@abv.bg>
2019-06-16 22:52:45 +0300

    Add exercises generics and iterators and comparators.

---
+++ C# Advanced/07. GenericsExercises/01. GenericBoxOfString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            var line = Console.ReadLine();
+
+
+
+            Console.WriteLine(new Box<string>(line));
+        }
+    }
+}
---
+++ C# Advanced/07. GenericsExercises/02. GenericBoxOfInteger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new Box<int>(num));
+        }
+    }
+}

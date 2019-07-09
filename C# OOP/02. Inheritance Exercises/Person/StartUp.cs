05d9efc88304728f6708809ec923d19f56ddcce2
HrBozhidarov <hristo_bozhidarov@abv.bg>
2019-07-09 08:19:15 +0300

    Add exercise - inheritance.

---
+++ C# OOP/02. Inheritance Exercises/Person/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            var child = new Child(name, age);
+
+            Console.WriteLine(child);
+        }
+    }
+}
